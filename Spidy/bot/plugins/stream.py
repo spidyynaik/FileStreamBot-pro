8092fd8529330527762899d76f3f1e98f0f1f3cd
Spidy <128451864+spidyynaik@users.noreply.github.com>
2023-06-24 18:13:07 +0530

    Update stream.py

--- Spidy/bot/plugins/stream.py
+++ Spidy/bot/plugins/stream.py
@@ -116,10 +116,10 @@ async def private_receive_handler(c: Client, m: Message):
         await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**𝚄𝚜𝚎𝚛 𝙸𝙳 :** `{str(m.from_user.id)}`", disable_web_page_preview=True)
 
 @StreamBot.on_callback_query(filters.regex(r'stream'))
-async def stream_callback_handler(c: Client, query: CallbackQuery):
+async def stream_callback_handler(c: Client, query: CallbackQuery, m: Message):
     try:
         data = query.data
-        log_msg = await Mesage.forward(chat_id=Var.BIN_CHANNEL)
+        log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         if query.data == "stream":
